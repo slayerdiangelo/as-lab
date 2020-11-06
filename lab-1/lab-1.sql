fbf3beec96cc95588f93d365d7300ce11fe4f277
slayerdiangelo99 <slayerdiangelo@gmail.com>
2020-11-06 13:39:18 +0530

    lab-1

---
+++ lab-1/lab-1.sql
@@ -0,0 +1,27 @@
+CREATE TABLE student (
+    sno INTEGER,
+    name CHAR(20),
+    marks INTEGER,
+    dept CHAR(5)
+);
+
+
+ALTER TABLE student
+ADD COLUMN age INTEGER;
+
+ALTER TABLE student
+MODIFY COLUMN dept CHAR(4);
+
+ALTER TABLE student
+DROP COLUMN marks;
+
+ALTER TABLE student
+RENAME TO students;
+
+INSERT INTO students
+VALUES(1,'ALAN','CSE',20);
+
+TRUNCATE students;
+
+DROP TABLE students;
+
